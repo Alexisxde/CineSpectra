556785c78a18b2b1c98f59fa7db6e8a02f367dfc
Alexisxde <olivarezalexis749@gmail.com>
2024-06-29 23:33:23 -0300

    page 404 not found 🆕

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,16 @@
+import { Link } from 'react-router-dom'
+
 export default function NotFound() {
-	return <h1>404 - Page Not Found</h1>
+	return (
+		<div className='-m-24 flex h-[100vh] flex-col items-center justify-center'>
+			<h1 className='text-3xl'>Error 404</h1>
+			<p className='animate-type pr-1.5 text-lg'>error permission denied</p>
+			<span>
+				go to{' '}
+				<Link className='underline' to='/'>
+					home
+				</Link>
+			</span>
+		</div>
+	)
 }
